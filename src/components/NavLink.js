8c088d72e03943f60f69dc01704a4b66098c8319
ClaytonxParrish <105694697+ClaytonxParrish@users.noreply.github.com>
2023-06-08 20:28:41 -0700

    Updated

--- src/components/NavLink.js
+++ src/components/NavLink.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 function NavLink(props) {
-
+    const style = useSelector(state => state.style)
     return (
-        <div className="NavLink" style={{'backgroundColor': 'white'}}>
+        <div className="NavLink" style={{'backgroundColor': style.color4}}>
             <p>{props.content}</p>
         </div>
     )
